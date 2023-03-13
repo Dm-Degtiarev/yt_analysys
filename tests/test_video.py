33241c27a1ed4fdfea14613eb38123434bc0f943
Dmitrii Degtiarev <9774385859@mail.ru>
2023-03-13 16:43:18 +0300

    Выполнено ДЗ урока 16.1 - Исключения

--- tests/test_video.py
+++ tests/test_video.py
@@ -2,6 +2,7 @@ from classes.video import Video
 
 
 video_1 = Video('9lO06Zxhu88')
+video_2 = Video('BrokenID')
 
 def test_init():
     """Тестирует инициализацию"""
@@ -10,6 +11,9 @@ def test_init():
     #Динамичные атрибуты. Проверяем что просто объявляются
     assert video_1.view_cnt
     assert video_1.like_cnt
+    # Проверяем логику работы при неправильном ID
+    assert video_2.video == video_2.video_name == video_2.like_cnt == video_2.view_cnt == None
+    assert video_2.video_id == 'BrokenID'
 
 
 def test_str():
